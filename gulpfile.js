3da02ae84b6aaca99629a8e102fe519602a7b116
Omar Abusheikh <oabusheikh@utexas.edu>
2019-10-15 21:42:56 -0500

    restructuring directories for npm

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+"use strict";
+
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cssnano = require("cssnano");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const webpack = require("webpack");
+const webpackconfig = require("./webpack.config.js");
+const webpackstream = require("webpack-stream");
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean assets - Remove previously compiled files before re-compiling
+function clean() {
+  return del(["./static/"]);
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./assets/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" })) // converts into static css; 
+    .pipe(gulp.dest("./static/css/")) 
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./static/css/"))
+    .pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+  return gulp
+    .src(["./assets/js/**/*", "./gulpfile.js"])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return (
+    gulp
+      .src(["./assets/js/**/*"])
+      .pipe(plumber())
+      .pipe(webpackstream(webpackconfig, webpack))
+      // folder only, filename is specified in webpack config
+      .pipe(gulp.dest("./static/js/"))
+      .pipe(browsersync.stream())
+  );
+}
+
+//
+function copyLibraries(){
+  return (gulp.src("./node_modules/jquery/dist/jquery.min.js").pipe(gulp.dest("./static/js"))); // will install the latest version of jquery)
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./assets/scss/**/*", css);
+  gulp.watch("./assets/js/**/*", gulp.series(scriptsLint, scripts));
+  gulp.watch("./index.html", browserSyncReload);
+}
+
+// define complex tasks
+const js = gulp.series(scriptsLint, scripts, copyLibraries);
+const build = gulp.series(clean, copyLibraries, gulp.parallel(css, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
